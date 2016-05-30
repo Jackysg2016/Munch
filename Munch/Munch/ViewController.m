ed83e3c42b6a7b01616e388f60ef12c38c4e207c
taylorbenna <taylor@benna.ca>
2016-05-30 12:06:24 -0700

    Added gitignore file

--- Munch/Munch/ViewController.m
+++ Munch/Munch/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CLLocation *lastLocation;
 
 @end
 
@@ -16,12 +20,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [self.locationManager requestWhenInUseAuthorization];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [self.locationManager startUpdatingLocation];
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    CLLocation *currentLocation = [locations firstObject];
+    
+    if (self.lastLocation == nil) {
+        
+        CLGeocoder *coder = [[CLGeocoder alloc] init];
+        
+        [coder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+            
+            
+            NSString *urlString = @"https://api.yelp.com/v2/search?term=food&ll=37.788022,-122.399797";
+            
+            NSURLSession *session = [NSURLSession sharedSession];
+            
+            NSURLSessionTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:urlString] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                
+                
+                
+            }];
+            
+            [dataTask resume];
+            
+            
+        }];
+        
+
+
+        
+        
+    }
+    
+    self.lastLocation = currentLocation;
+    
 }
 
 @end
