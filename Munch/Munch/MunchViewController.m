fef13d367cc47a7f2b6bb9a608f38fa676a1f41e
enochng1 <enochng1@gmail.com>
2016-06-01 12:41:24 -0700

    updated MunchController View Filter Tab with working screen

--- Munch/Munch/MunchViewController.m
+++ Munch/Munch/MunchViewController.m
@@ -25,6 +25,12 @@
 
 @property (weak, nonatomic) IBOutlet RestaurantCardFactory *restaurantFactory;
 
+@property (weak, nonatomic) IBOutlet UIView *filterView;
+@property (weak, nonatomic) IBOutlet UIButton *filterTabButton;
+@property (weak, nonatomic) IBOutlet UIView *dimView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *filterHeightConstraint;
+
+
 @end
 
 
@@ -135,5 +141,50 @@
     return paramDictionary;
 }
 
+- (IBAction)pressedFilterTab:(UIButton *)sender {
+    
+        if(self.filterHeightConstraint.constant == 0.0){
+    
+            self.filterHeightConstraint.constant = self.view.frame.size.height * 0.7;
+    
+            [UIView animateWithDuration:0.5
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseInOut
+                             animations:^{
+                                   self.dimView.alpha = 0.2;
+    
+                             }
+                             completion:^(BOOL finished) {
+    
+                             }];
+    
+        } else {
+            
+            self.filterHeightConstraint.constant = 0.0;
+    
+            [UIView animateWithDuration:0.5
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseInOut
+                             animations:^{
+                                  self.dimView.alpha = 0.0;
+    
+                             }
+                             completion:^(BOOL finished) {
+    
+                         }];
+         }
+    
+        [UIView animateWithDuration:0.5
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             [self.view layoutIfNeeded];
+                         }
+                         completion:^(BOOL finished) {
+                             
+                         }];
+    
+
+}
 
 @end
