a7ea9f39d50f7d803efddf036123fa3e407e091f
enochng1 <enochng1@gmail.com>
2016-06-01 19:38:06 -0700

    added new detail + layout + phone call function + back and forth munch view and detailed munch view, need to start working on market framework and taking data from restaurant

---
+++ Munch/Munch/ContainerClassViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContainerClassViewController.h
+//  Munch
+//
+//  Created by Enoch Ng on 2016-06-01.
+//  Copyright © 2016 Enoch Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContainerClassViewController : UIViewController
+
+@end
---
+++ Munch/Munch/ContainerClassViewController.m
@@ -0,0 +1,19 @@
+//
+//  ContainerClassViewController.m
+//  Munch
+//
+//  Created by Enoch Ng on 2016-06-01.
+//  Copyright © 2016 Enoch Ng. All rights reserved.
+//
+
+#import "ContainerClassViewController.h"
+
+@implementation ContainerClassViewController
+
+-(void)viewDidLoad{
+    
+
+
+}
+
+@end
