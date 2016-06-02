be7e3249c70aba4ecfb099a9dc88bf91b8117330
taylorbenna <taylor@benna.ca>
2016-06-02 11:11:08 -0700

    changed Restaurant Model

--- Munch/Munch/Restaurant+CoreDataProperties.m
+++ Munch/Munch/Restaurant+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  Restaurant+CoreDataProperties.m
 //  Munch
 //
-//  Created by Taylor Benna on 2016-06-01.
+//  Created by Taylor Benna on 2016-06-02.
 //  Copyright © 2016 Enoch Ng. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,13 +14,16 @@
 @implementation Restaurant (CoreDataProperties)
 
 @dynamic address;
+@dynamic categories;
 @dynamic latitude;
 @dynamic longitude;
 @dynamic name;
 @dynamic pricing;
 @dynamic rating;
 @dynamic verbalAddress;
-@dynamic categories;
+@dynamic distance;
+@dynamic ratingURL;
+@dynamic phoneNumber;
 @dynamic deals;
 @dynamic imageURLs;
 
